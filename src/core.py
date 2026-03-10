b2dbc17cb5b4ce5aac532c5a8c68b6e56445ab2d
Alexey-Nikolaevich-B <alexey.bardushko@gmail.com>
2026-03-11 00:28:08 +0300

    Removed redundant files

--- src/core.py
+++ src/core.py
@@ -1,10 +1,8 @@
 import sys
 from typing import List, Dict, Any
 
-from .sensor_library_utils import get_sensor_types, get_sensors_name_by_type
 from .gazebo_simulator import Simulator
-from .test_utils import load_test_functions
-from .sensors import Sensor
+from .sensors import REGISTRY, Sensor
 
 from config import CONFIG
 
@@ -13,27 +11,24 @@ class Core:
     def __init__(self) -> None:
         self.simulator = Simulator(CONFIG)
 
-
     def prepare_simulator(self) -> None:
         self.simulator.launch()
 
-
     def get_sensor_types(self) -> List[str]:
-        return get_sensor_types()
+        return sorted({sensor_type for sensor_type, _ in REGISTRY.keys()})
 
-
-    def get_sensors_name_by_type(self, sensor_type: str) -> List[str]:
-        return get_sensors_name_by_type(sensor_type)
-
+    def get_sensors_by_type(self, sensor_type: str) -> List[str]:
+        return sorted({name for registered_type, name in REGISTRY.keys() if registered_type == sensor_type})
 
     def get_tests(self, sensor: Sensor) -> Dict[str, Any]:
-        return load_test_functions(sensor)
+        return {
+            attr: getattr(sensor, attr)
+            for attr in dir(sensor)
+            if attr.endswith('_test') and callable(getattr(sensor, attr))
+        }
 
-
-    def capture_data(self, sensor: Sensor, world_path, **kwargs) -> any:
-        data = sensor.capture_data(self.simulator, world_path=world_path, **kwargs)
-        return data
-
+    def capture_data(self, sensor: Sensor, world_path, **kwargs) -> Any:
+        return sensor.capture_data(self.simulator, world_path=world_path, **kwargs)
 
     def kill(self) -> None:
         self.simulator.kill()
